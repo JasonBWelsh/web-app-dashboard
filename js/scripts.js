8492a86a8f36c3875f3e0b8e30bd6582d7c2847e
jason welsh <jasonbwelsh@gmail.com>
2017-06-10 14:16:40 -0400

    Alert Box styling and list functionality
    Basic alert styling and functionality added. Ability to remove
    individual notifications added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 
 // Bell Alert
-
+const body = document.querySelector('body');
 const bellLI = document.querySelector('.header-list li:first-child');
 const indicator = bellLI.querySelector('.bell-indicator');
 const alertBox = document.querySelector('.alert-box');
+const alertUL = alertBox.querySelector('ul');
+const alertCloseBtn = alertBox.querySelector('.alert-close-btn');
 
-bellLI.addEventListener('click', (e) => {
-
+// Check if notifications are present and display alert box if so
+bellLI.addEventListener('click', (e) => {                     
 	if (bellLI.classList.contains('notifications-ready')) {
 		bellLI.classList.remove('notifications-ready');
 		alertBox.style.display = 'inline-block';
@@ -15,6 +17,24 @@ bellLI.addEventListener('click', (e) => {
 		bellLI.classList.add('notifications-ready');
 		alertBox.style.display = 'none';
 	}
-
 });
 
+// click close button to close alert
+alertCloseBtn.addEventListener('click', () => {  
+	alertBox.style.display = 'none';
+});
+
+/*body.addEventListener('click', () => {      // click anywhere on body to remove alert box
+	if (alertBox.style.display !== 'none') {
+		alertBox.style.display = 'none';
+	}
+});*/
+
+// remove alert LI buttons
+alertUL.addEventListener('click', (e) => {
+	if (e.target.classList.contains('remove-btn')) {
+		let btn = e.target;
+		let li = btn.parentNode;
+		alertUL.removeChild(li);
+	}
+});
